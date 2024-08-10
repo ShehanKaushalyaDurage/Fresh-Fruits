971e206b272cb3c8aca9e47f58c7ad0da34823f0
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-11 00:40:46 +0530

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#de2c4d",
+        secondary: "#fb923c",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        averia: ["Averia Serif Libre", "serif"],
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
