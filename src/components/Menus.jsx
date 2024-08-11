ca886936c526a61f142617c67a51b363f1668b45
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-12 00:22:39 +0530

    menu part animation done

--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -3,6 +3,8 @@ import Fruit1 from "../assets/fruits/apple.png";
 import Fruit2 from "../assets/fruits/orange.png";
 import Fruit3 from "../assets/fruits/avocado.png";
 import Fruit4 from "../assets/fruits/cherry.png";
+import { motion } from "framer-motion";
+import { FadeLeft } from "../utility/animation";
 
 const menusData = [
   {
@@ -11,6 +13,7 @@ const menusData = [
     link: "/",
     price: "$2.00",
     img: Fruit1,
+    delay : 0.3,
   },
   {
     id: 2,
@@ -18,6 +21,7 @@ const menusData = [
     link: "/",
     price: "$4.00",
     img: Fruit2,
+    delay : 0.6,
   },
   {
     id: 3,
@@ -25,6 +29,7 @@ const menusData = [
     link: "/",
     price: "$6.00",
     img: Fruit3,
+    delay : 0.9,
   },
   {
     id: 4,
@@ -32,22 +37,32 @@ const menusData = [
     link: "/",
     price: "$5.00",
     img: Fruit4,
+    delay : 1.2,
   },
 ];
 const Menus = () => {
   return (
     <section>
       <div className="container pt-12 pb-20">
-        <h1 className="text-2xl font-bold text-left pb-10 uppercase"> Our Menu</h1>
+        <motion.h1 
+          initial={{ opacity: 0, x: -200}}
+          whileInView={{ opacity: 1, x: 0}}  
+          transition={{ duration: 1, delay: 0.3 }}
+        className="text-2xl font-bold text-left pb-10 uppercase"> Our Menu</motion.h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
             {menusData.map((menu) => (
-                <div className="bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3">
+                <motion.div
+                variants={FadeLeft(menu.delay)}
+                initial="hidden"
+                whileInView={"visible"}
+                whileHover={{scale: 1.1}}
+                className="bg-white rounded-3xl px-4 py-2 shadow-[0_0_22px_0_rgba(0,0,0,0.15)] flex flex-row justify-around items-center gap-3">
                     <img src={menu.img} className="w-[60px] mb-4 scale-110 transform -translate-y-6"/>
                     <div>
                         <h1 className="text-lg font-semibold">{menu.title}</h1>
                         <p className="text-lg font-semibold text-secondary">{menu.price}</p>
                     </div>
-                </div>
+                </motion.div>
             ))}
         </div>
       </div>
