08337930c1340423ab351cb0f795d656046f6380
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-12 00:24:41 +0530

    4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,9 @@ const Hero = () => {
   return (
     <section>
       <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] relative">
+        
         {/* Brand Info */}
+
         <div className="flex flex-col justify-center py-14 md:py-0 relative z-10">
           <div className="text-center md:text-left space-y-6  lg:max-w-[400px]">
             <motion.h1
