45c23821f7f3a31fc4375beae71bf8f42f4138f8
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-11 21:24:03 +0530

    hero design done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { IoBagHandleOutline } from "react-icons/io5";
+import HeroPng from "../assets/fruit-plate.png";
+import LeafPng from "../assets/leaf.png";
 
 const Hero = () => {
   return (
     <section>
-      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] ">
+      <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px] relative">
         {/* Brand Info */}
         <div className="flex flex-col justify-center py-14 md:py-0 relative z-10">
           <div className="text-center md:text-left space-y-6  lg:max-w-[400px]">
@@ -13,11 +16,17 @@ const Hero = () => {
             <p className="text-2xl tracking-wide">Order Now For Fresh Healthy Life</p>
             <p className="text-gray-400">Embark on a journey toward a healthier, more vibrant life by choosing the freshest fruits delivered straight to your doorstep.</p>
             <div className="flex justify-center md:justify-start">
-            <button className="primary-btn">Order Now</button>
+            <button className="primary-btn flex items-center gap-2"><span><IoBagHandleOutline/> </span>Order Now</button>
             </div>
           </div>
         </div>
         {/* Hero Images */}
+        <div className="flex justify-center items-center">
+            <img  src={HeroPng} className="w-[350px] md:w-[550px] drop-shadow"/>
+        </div>
+        <div className="absolute top-14 md:top-0 right-1/2 blur-sm opacity-80 rotate-[40deg]">
+            <img src={LeafPng} className="w-full md:max-w-[300px]"/>
+        </div>
       </div>
     </section>
   );
