43b8380ad8d3a7a2aec874d58713df97c05df22b
shehan kaushalya <shehankaushalyadurage@gmail.com>
2024-08-13 00:43:47 +0530

    ban 3

--- src/components/Banner3.jsx
+++ src/components/Banner3.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import BannerPng from "../assets/fruits-splash.png";
+import { motion } from "framer-motion";
+import { FadeUp } from "../utility/animation";
 
 const Banner3 = () => {
   return (
-    <div>
-      Banner3
+    <section className="bg-secondary/10">
+    <div className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14 overflow-y-hidden">
+      {/* banner image */}
+      <div className="flex justify-center items-center">
+        <motion.img
+        initial={{opacity:0, scale: 0.5}}
+        whileInView={{opacity:1,scale:1}}
+        transition={{type:"spring",stiffness:100,delay:0.2}}
+        viewport={{once:true}}
+          src={BannerPng}
+          className="w-[300px] md:max-w-[400px] h-full object-cover drop-shadow"
+        />
+      </div>
+      {/* brand info */}
+      <div className="flex flex-col justify-center ">
+        <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
+          <motion.h1 
+          variants={FadeUp(0.5)}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{once:true}}
+          className="text-3xl lg:text-5xl font-bold uppercase"> Brand Info</motion.h1>
+          <motion.p
+           variants={FadeUp(0.7)}
+           initial="hidden"
+           whileInView="visible"
+           viewport={{once:true}}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+            eaque delectus? Velit molestias consectetur voluptates facere
+            pariatur! Consequuntur officia, laboriosam voluptate nobis libero
+            mollitia expedita a dolorum commodi natus nam.
+          </motion.p>
+          <motion.p
+           variants={FadeUp(0.9)}
+           initial="hidden"
+           whileInView="visible"
+           viewport={{once:true}}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+            eaque delectus? Velit molestias consectetur voluptates facere
+            pariatur! Consequuntur officia, laboriosam voluptate nobis libero
+            mollitia expedita a dolorum commodi natus nam.
+          </motion.p>
+          
+          <motion.div
+            variants={FadeUp(1.1)}
+            initial="hidden"
+            animate="visible"
+            className="flex justify-center md:justify-start"
+          >
+            <button className="primary-btn ">
+              Learn more
+            </button>
+          </motion.div>
+        </div>
+      </div>
     </div>
+  </section>
   )
 }
 
